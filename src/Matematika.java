1d4f86247f2bf4cb07b951ec1c028aa1a1c77f52
Berigoo <muhamadrizqifajri2@gmail.com>
2023-10-18 23:37:35 +0700

    initial commit

---
+++ src/Matematika.java
@@ -0,0 +1,54 @@
+public class Matematika implements MatematikaInterface {
+    Matematika(){
+        System.out.println("There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n" +
+                "B-I-N-G-O  B-I-N-G-O  B-I-N-G-O\n" +
+                "And Bingo was his name-o.\n" +
+                "There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n\n" +
+                "(clap)-I-N-G-O\n" +
+                "(clap)-I-N-G-O\n" +
+                "(clap)-I-N-G-O\n" +
+                "And Bingo was his name-o.\n\n" +
+                "There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n" +
+                "(clap)-(clap)-N-G-O\n" +
+                "(clap)-(clap)-N-G-O\n" +
+                "(clap)-(clap)-N-G-O\n" +
+                "And Bingo was his name-o.\n\n" +
+                "There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n" +
+                "(clap)-(clap)-(clap)-G-O\n" +
+                "(clap)-(clap)-(clap)-G-O\n" +
+                "(clap)-(clap)-(clap)-G-O\n" +
+                "And Bingo was his name-o.\n\n" +
+                "There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n" +
+                "(clap)-(clap)-(clap)-(clap)-O\n" +
+                "(clap)-(clap)-(clap)-(clap)-O\n" +
+                "(clap)-(clap)-(clap)-(clap)-O\n" +
+                "And Bingo was his name-o.\n\n" +
+                "There was a farmer who had a dog,\n" +
+                "And Bingo was his name-o.\n" +
+                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+                "And Bingo was his name-o.\n\n");
+    }
+    @Override
+    public int pertambahan(int a, int b){
+        return a + b;
+    }
+    @Override
+    public int pengurangan(int a, int b){
+        return a - b;
+    }
+    @Override
+    public int perkalian(int a, int b) {
+        return a * b;
+    }
+    @Override
+    public int pembagian(int a, int b){
+        return a / b;
+    }
+}

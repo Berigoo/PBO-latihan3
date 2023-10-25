a97b925373b99d849ad4a689eef3696a5edc4331
Berigoo <muhamadrizqifajri2@gmail.com>
2023-10-25 19:55:50 +0700

    intial commit

--- src/Matematika.java
+++ src/Matematika.java
@@ -1,39 +1,39 @@
 public class Matematika implements MatematikaInterface {
     Matematika(){
-        System.out.println("There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n" +
-                "B-I-N-G-O  B-I-N-G-O  B-I-N-G-O\n" +
-                "And Bingo was his name-o.\n" +
-                "There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n\n" +
-                "(clap)-I-N-G-O\n" +
-                "(clap)-I-N-G-O\n" +
-                "(clap)-I-N-G-O\n" +
-                "And Bingo was his name-o.\n\n" +
-                "There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n" +
-                "(clap)-(clap)-N-G-O\n" +
-                "(clap)-(clap)-N-G-O\n" +
-                "(clap)-(clap)-N-G-O\n" +
-                "And Bingo was his name-o.\n\n" +
-                "There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n" +
-                "(clap)-(clap)-(clap)-G-O\n" +
-                "(clap)-(clap)-(clap)-G-O\n" +
-                "(clap)-(clap)-(clap)-G-O\n" +
-                "And Bingo was his name-o.\n\n" +
-                "There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n" +
-                "(clap)-(clap)-(clap)-(clap)-O\n" +
-                "(clap)-(clap)-(clap)-(clap)-O\n" +
-                "(clap)-(clap)-(clap)-(clap)-O\n" +
-                "And Bingo was his name-o.\n\n" +
-                "There was a farmer who had a dog,\n" +
-                "And Bingo was his name-o.\n" +
-                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
-                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
-                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
-                "And Bingo was his name-o.\n\n");
+//        System.out.println("There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n" +
+//                "B-I-N-G-O  B-I-N-G-O  B-I-N-G-O\n" +
+//                "And Bingo was his name-o.\n" +
+//                "There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n\n" +
+//                "(clap)-I-N-G-O\n" +
+//                "(clap)-I-N-G-O\n" +
+//                "(clap)-I-N-G-O\n" +
+//                "And Bingo was his name-o.\n\n" +
+//                "There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n" +
+//                "(clap)-(clap)-N-G-O\n" +
+//                "(clap)-(clap)-N-G-O\n" +
+//                "(clap)-(clap)-N-G-O\n" +
+//                "And Bingo was his name-o.\n\n" +
+//                "There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n" +
+//                "(clap)-(clap)-(clap)-G-O\n" +
+//                "(clap)-(clap)-(clap)-G-O\n" +
+//                "(clap)-(clap)-(clap)-G-O\n" +
+//                "And Bingo was his name-o.\n\n" +
+//                "There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n" +
+//                "(clap)-(clap)-(clap)-(clap)-O\n" +
+//                "(clap)-(clap)-(clap)-(clap)-O\n" +
+//                "(clap)-(clap)-(clap)-(clap)-O\n" +
+//                "And Bingo was his name-o.\n\n" +
+//                "There was a farmer who had a dog,\n" +
+//                "And Bingo was his name-o.\n" +
+//                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+//                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+//                "(clap)-(clap)-(clap)-(clap)-(clap)\n" +
+//                "And Bingo was his name-o.\n\n");
     }
     @Override
     public int pertambahan(int a, int b){
---
+++ src/MatematikaInheritance.java
@@ -0,0 +1,5 @@
+public class MatematikaInheritance {
+    public static void main (String[] args) {
+        Matematika Math1 = new Matematika();
+    }
+}
